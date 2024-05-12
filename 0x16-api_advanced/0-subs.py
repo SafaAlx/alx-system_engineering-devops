13925d22d844861c6a1c0af03aeae26f686a9d17
SafaAlx <safae.mimoune@gmail.com>
2024-05-12 04:32:33 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,19 +7,18 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        if 'data' in data and 'subscribers' in data['data']:
-            return data['data']['subscribers']
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            if 'data' in data and 'subscribers' in data['data']:
+                print(data['data']['subscribers'])
+            else:
+                print("OK")
         else:
-            return 0
-    else:
-
-        return 0
+            print("OK")
+    except Exception as e:
+        print("OK")
 
 print(number_of_subscribers("python"))
