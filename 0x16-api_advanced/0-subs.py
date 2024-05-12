f5a0f007e6ecde1d8d4ffd2061139ac16fae1a96
SafaAlx <safae.mimoune@gmail.com>
2024-05-12 04:27:53 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ Script that queries subscribers on a given Reddit subreddit.
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/subreddit/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     response = requests.get(url, headers=headers)
